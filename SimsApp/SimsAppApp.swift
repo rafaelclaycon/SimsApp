05f067fca8f330c0ab23445cc786c83a1cc68e65
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-11-21 10:53:23 -0300

    Initial commit

---
+++ SimsApp/SimsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimsAppApp.swift
+//  SimsApp
+//
+//  Created by Rafael Schmitt on 23/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SimsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
